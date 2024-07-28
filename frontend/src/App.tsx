a1b344f34e7104a48fd74e183a6aeb886917a993
Spyabo <reesewright02@protonmail.com>
2024-07-28 18:05:12 +0100

    Add tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="py-2 px-4 bg-green-500 hover:bg-blue-700 text-white font-bold rounded" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
